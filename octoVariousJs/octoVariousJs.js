5c56f71ff07f0843910dd3800ed00eca5f3d20db
Paul Etienney <p.etienney@gmail.com>
2013-03-29 17:41:05 +0100

    add black transaparent 1px images in various opacity

--- octoVariousJs/octoVariousJs.js
+++ octoVariousJs/octoVariousJs.js
@@ -1,7 +1,18 @@
-// Handle smooth scrolling
-// NB : requires scrollTo plugin (https://github.com/flesler/jquery.scrollTo)
+
+$(document).ready(function() {
+	// Handle smooth scrolling
+	// NB : requires scrollTo plugin (https://github.com/flesler/jquery.scrollTo)
     $(".smoothScroll").click(function(event){
         event.preventDefault();
         var targetName = $(this).attr('href');
         $('body').scrollTo($(targetName), 800);
     });
+
+	// /////////////////////////
+    // POPUP OPENER
+    // /////////////////////////
+    $('.popup').click(function(){
+        var href = $(this).attr('href');
+        return !window.open(href, '', 'width=500,height=500')
+    });
+});
